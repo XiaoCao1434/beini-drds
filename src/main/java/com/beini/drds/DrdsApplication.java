c5f248cacdb24d79e25923ece5ea9ed463cb1343
Administrator <Administrator@lb_chen>
2018-07-19 10:23:11 +0800

    dd

--- src/main/java/com/beini/drds/DrdsApplication.java
+++ src/main/java/com/beini/drds/DrdsApplication.java
@@ -2,7 +2,6 @@ package com.beini.drds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,10 +13,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = "com.beini")
 @ImportResource(locations = { "classpath:druid-spring.xml" })
 public class DrdsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DrdsApplication.class, args);
 	}
